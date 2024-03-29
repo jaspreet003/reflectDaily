be79eff3ccc875aac3b11106e8eb55e8521d37bb
jaspreet <ghotra.jaspreet003@gmail.com>
2024-03-29 13:16:50 -0400

    go next previous in carousel with button

--- reflectDaily/reflectDaily/Main/journal/NewJournalPage.xaml.cs
+++ reflectDaily/reflectDaily/Main/journal/NewJournalPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using reflectDaily.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,17 +30,91 @@ namespace reflectDaily.Main.journal
             titleView.Margin = new Thickness(30, 0, 30, 0);
             NavigationPage.SetTitleView(this, titleView);
 		}
+        List<JournalQuestion> questionList = new List<JournalQuestion>
+        {
+            new JournalQuestion
+            {
+                questionNumber = "Question - 1",
+                questionDetail = "how many meals you eat today ?",
+                options = new List<String> {
+                "1 meal",
+                "2 meal",
+                "3 meal",
+                "4 meal",
+                }
 
+            },
+
+            new JournalQuestion
+            {
+                questionNumber = "Question - 2",
+                questionDetail =  "How much you exercise today?",
+                options = new List<String> {
+                 "10 Min",
+                "20 Min",
+                "30 Min",
+                "40 Min",
+                "50 Min",
+                "1 Hour",
+                "1- 2 Hour",
+                "More Than 2 Hours",
+                "Did not practise today"
+                }
+
+            }
+            
+
+};
         protected override void OnAppearing()
         {
             base.OnAppearing();
             if (Parent is NavigationPage navigationPage)
             {
                 navigationPage.BarBackgroundColor = (Color)Application.Current.Resources["primary"];
-                navigationPage.BarTextColor = Color.White; // Adjust as needed
+                navigationPage.BarTextColor = Color.White;
             }
 
+            carouselQuestion.ItemsSource = questionList;
 
+        }
+
+        private void carouselQuestion_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Clicked(object sender, EventArgs e)
+        {
+            int nextPosition = carouselQuestion.Position + 1;
+
+            if (nextPosition < ((carouselQuestion.ItemsSource as IEnumerable<object>).Count() - 1) )
+            {
+                carouselQuestion.Position = nextPosition;
+            }
+            else if(nextPosition < (carouselQuestion.ItemsSource as IEnumerable<object>).Count())
+            {
+                carouselQuestion.Position = nextPosition;
+                var nextButton = sender as Button;
+                nextButton.Text = "SUBMIT";
+            }
+            else
+            {
+                Navigation.PushAsync(new SuccessPage());
+            }
+        }
+
+        private void PreviousButton_Clicked(object sender, EventArgs e)
+        {
+            int prevPosition = carouselQuestion.Position - 1;
+            if (prevPosition > 0)
+            {
+                carouselQuestion.Position = prevPosition;
+            }
+            else
+            {
+                var previousButton = sender as Button;
+                previousButton.IsEnabled = false;
+            }
         }
     }
 }
--- reflectDaily/reflectDaily/Model/JournalQuestion.cs
+++ reflectDaily/reflectDaily/Model/JournalQuestion.cs
@@ -6,25 +6,9 @@ namespace reflectDaily.Model
 {
     public class JournalQuestion
     {
-        public int questionNumber { get; set; }
+        public String questionNumber { get; set; }
         public String questionDetail { get; set; }
         public List<String> options { get; set; }
 
-        public JournalQuestion() {
-
-            this.questionNumber = 1;
-            this.questionDetail = "How much you exercise today?";
-            this.options = new List<String>() { 
-                "10 Min",
-                "20 Min",
-                "30 Min",
-                "40 Min",
-                "50 Min",
-                "1 Hour",
-                "1- 2 Hour",
-                "More Than 2 Hours",
-                "Did not practise today"
-            };
-        }
     }
 }
