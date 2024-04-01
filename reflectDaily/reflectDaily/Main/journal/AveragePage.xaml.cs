6ce78fc968acb74c769237db27ce96b4d1005327
jaspreet <ghotra.jaspreet003@gmail.com>
2024-04-01 13:57:54 -0400

    average setup fixed

--- reflectDaily/reflectDaily/Main/journal/AveragePage.xaml.cs
+++ reflectDaily/reflectDaily/Main/journal/AveragePage.xaml.cs
@@ -16,5 +16,35 @@ namespace reflectDaily.Main.journal
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void StartDateButton_Clicked(object sender, EventArgs e)
+        {
+            calendar.IsVisible = true;  
+        }
+
+        private void EndDateButton_Clicked(object sender, EventArgs e)
+        {
+            calendar.IsVisible = true;
+        }
+
+        private void CheckButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AverageResultPage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void LastWeekAvgButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync (new AverageResultPage());
+        }
+
+        private void LastMonthButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AverageResultPage());
+        }
+    }
 }
