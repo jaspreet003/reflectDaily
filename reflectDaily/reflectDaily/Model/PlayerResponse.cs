27de7242d05603ee2fb2c1b0706425f0daf42c4b
jaspreet <ghotra.jaspreet003@gmail.com>
2024-04-04 13:30:37 -0400

    playerResponse Class created

---
+++ reflectDaily/reflectDaily/Model/PlayerResponse.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reflectDaily.Model
+{
+    public class PlayerResponse
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string QuestionId { get; set; }
+
+        public string SelectedOption { get; set; }
+
+        public DateTime ResponseDate { get; set; }
+    }
+}
